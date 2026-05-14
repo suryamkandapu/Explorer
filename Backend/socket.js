36813efb5969f8c840d7bb97c9c6552fd0a0dfbc
Suryam205 <suryamkandapu205@gmail.com>
2026-05-14 13:03:12 +0530

    fixed socket io cors

--- Backend/socket.js
+++ Backend/socket.js
@@ -5,7 +5,10 @@ let io;
 const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: [
+        "http://localhost:5173",
+        "https://explorer-k7p9.onrender.com",
+      ],
       methods: ["GET", "POST"],
       credentials: true,
     },
@@ -15,7 +18,10 @@ const initSocket = (server) => {
 };
 
 const getIO = () => {
-  if (!io) throw new Error("Socket.io not initialized!");
+  if (!io) {
+    throw new Error("Socket.io not initialized!");
+  }
+
   return io;
 };
 
